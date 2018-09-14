3e7bd0a0e72c14b2f08bb21c0055dc9872345789
Derperlerp <Derperlerp26@users.noreply.github.com>
2018-09-14 09:14:18 -0500

    File loader and tempRole
    Condensed the file loader and changed the structure of some of the timed objects. Added tempRole().

--- BoomBot2.py/main.py
+++ BoomBot2.py/main.py
@@ -22,7 +22,7 @@ else:
 
 
 # Bot and client
-Client = discord.Client()
+client = discord.Client()
 bot = commands.Bot(command_prefix = settings.prefix, case_insensitive = True, description = settings.description)
 
 # Token checker
@@ -53,95 +53,53 @@ class ObjStore(object):
                 lFile = open(loadFrom, 'w').write('')
             elif os.path.getsize(loadFrom) > 0:
                 with open(loadFrom, 'rb') as lFile:
-                    self()[loadFrom] = pickle.load(lFile)
+                    setattr(self, loadTo, pickle.load(lFile))
                 lFile.close()
             print("{} loaded.".format(loadFrom))
-            print(self()[loadFrom])
+            print(getattr(self, loadTo))
         except Exception as exce:
             print("Error when loading from {}.".format(loadFrom))
             print(exce)
-    def loadTempBans(self):
-        try:
-            if not os.path.isfile('tempBans.txt'):
-                tBans = open('tempBans.txt', 'w').write('')
-            elif os.path.getsize('tempBans.txt') > 0:
-                with open('tempBans.txt', 'rb') as tBans:
-                    self.tBanList = pickle.load(tBans)
-                tBans.close()
-            print("TempBans loaded.")
-            print(self.tBanList)
-        except Exception as exce:
-            print("Error in loadTempBans.")
-            print(exce)
-    def loadTempRoles(self):
-        try:
-            if not os.path.isfile('tempRoles.txt'):
-                tRoles = open('tempRoles.txt', 'w').write('')
-            elif os.path.getsize('tempRoles.txt') > 0:
-                with open('tempRoles.txt', 'rb') as tRoles:
-                    self.tRoleList = pickle.load(tRoles)
-                tRoles.close()
-            print("TempRoles loaded.")
-            print(self.tRoleList)
-        except Exception as exce:
-            print("Error in loadTempRoles.")
-            print(exce)
-    def loadPersistRoles(self):
-        try:
-            if not os.path.isfile('persistRoles.txt'):
-                pRoles = open('persistRoles.txt', 'w').write('')
-            elif os.path.getsize('persistRoles.txt') > 0:
-                with open('persistRoles.txt', 'rb') as pRoles:
-                    self.pRoleList = pickle.load(pRoles)
-                pRoles.close()
-            print("PersistRoles loaded.")
-            print(self.pRoleList)
-        except Exception as exce:
-            print("Error in loadPersistRoles.")
-            print(exce)
 
 class TempBan(object):
-    def __init__(self, user, duration, reason, startTime = round(time.time()), guild):
+    def __init__(self, user, duration, reason, guildID, startTime = round(time.time())):
         self.user = user
         self.duration = duration
         self.reason = reason
         self.startTime = startTime
-        self.guild = guild
+        self.guildID = guildID
 
 class TempRole(object):
-    def __init__(self, userID, duration, startTime = round(time.time()), guild):
+    def __init__(self, userID, duration, roleID, guildID, startTime = round(time.time())):
         self.userID = userID
         self.duration = duration
-        self.roles = []
+        self.roleID = roleID
         self.startTime = startTime
-        self.guild = guild
+        self.guildID = guildID
 
 class PersistRole(object):
-    def __init__(self, userID, guild):
+    def __init__(self, userID, roleID, guildID):
         self.userID = userID
         self.duration = duration
-        self.roles = []
-        self.guild = guild
+        self.roleID = roleID
+        self.guildID = guildID
 
 async def checkTime():
     """Checks all temp stuff and persist stuff."""
     while True:
         for tBan in objStore.tBanList:
             if (round(time.time()) - tBan.startTime) > (tBan.duration * 86400):
-                await tBan.guild.unban(tBan.user)
+                await client.get_guild(tBan.guildID).unban(tBan.userID)
         for tRole in objStore.tRoleList:
-            for role in tRole.roles:
-                if (round(time.time()) - tRole.startTime) > (tRole.duration * 86400):
-                    await tRole.guild.get_member(tRole.userID).remove_roles(tBan.role)
+            if (round(time.time()) - tRole.startTime) > (tRole.duration * 86400):
+                await client.get_guild(tRole.guildID).get_member(tRole.userID).remove_roles(tBan.roleID)
+            #elif client.get_role(tRole.roleID) not in client.get_guild(tRole.guildID).get_member(tRole.userID).roles:
+                #await client.get_guild(tRole.guildID).get_member(tRole.userID).add_roles(roleID)
         for pRole in objStore.pRoleList:
-            for role in pRole.roles:
-                if role not in pRole.guild.get_member(pRole.userID).roles:
-                    await pRole.guild.get_member(pRole.userID).add_roles(role)
+            if client.get_role(pRole.roleID) not in client.get_guild(pRole.guildID).get_member(pRole.userID).roles:
+                await client.get_guild(pRole.guildID).get_member(pRole.userID).add_roles(roleID)
         print("Time checked.")
-        time.sleep(900)
-
-async def converter(ctx, user):
-    return await commands.MemberConverter().convert(ctx, user)
+        time.sleep(60)
 
 
 objStore = ObjStore()
@@ -162,9 +120,7 @@ async def on_ready():
         print("\tID - {} : Name - {}".format(guild.id, guild.name))
     await bot.change_presence(activity = discord.Game(name = settings.currActivity))
     print("Playing: {}\n\n".format(settings.currActivity))
-    objStore.loadTempBans()
-    objStore.loadTempRoles()
-    objStore.loadPersistRoles()
+    objStore.loadFile('tempBans.txt', 'tBanList')
     #timeChecker.start()
 
 #@bot.event
@@ -186,71 +142,100 @@ async def about(ctx):
     await ctx.send(content = "About is still a work in progress, so fuck off until it\'s done")
 
 @bot.command()
-@commands.has_permissions(administrator = True)
 async def changeGame(ctx, *activity):
     """Changes what the bot is playing."""
-    embed = discord.Embed(title = "Command: changeGame", color = 0xff0000)
-    try:
-        settings.currActivity = ' '.join(activity)
-        updateSettings()
-        await bot.change_presence(activity = discord.Game(name = settings.currActivity))
-        embed.description = "Game successfully changed to *{}*".format(settings.currActivity)
-        embed.color = 0x00ff00
-    except:
-        embed.description = "An error has occurred when trying to change the game. If you're reading this, 1) Congrats on fucking up the bot, 2) Contact Xaereus about it."
-
-    except MissingPermissions:
+    if ctx.message.author.guild_permissions.administrator:
+        embed = discord.Embed(title = "Command: changeGame", color = 0xff0000)
+        try:
+            settings.currActivity = ' '.join(activity)
+            updateSettings()
+            await bot.change_presence(activity = discord.Game(name = settings.currActivity))
+            embed.description = "Game successfully changed to *{}*".format(settings.currActivity)
+            embed.color = 0x00ff00
+        except:
+            embed.description = "An error has occurred when trying to change the game. If you're reading this, 1) Congrats on fucking up the bot, 2) Contact Xaereus about it."
+    else:
         embed.description = "You do not have permission to use this command. Begone, ***thot***."
     await ctx.send(embed = embed)
 
 @bot.command()
-@commands.has_permissions(kick_members = True)
-async def tempBan(ctx, user, duration, *reason):
+async def tempBan(ctx, user : discord.Member, duration, *reason):
     """Tempbans a mothafucka."""
     embed = discord.Embed(title = "Command: tempBan", color = 0xff0000)
-    try:
-        for tBan in objStore.tBanList:
-            if tBan.user == converter(ctx, user):
-                embed.description = "This user is already in the banlist. Get your shit together, kachigga."
-        else:
-            try:
-                objStore.tBanList.append(TempBan(converter(ctx, user), duration, ' '.join(reason), round(time.time()), ctx.message.guild))
-                print(objStore.tBanList)
-                #await ctx.message.mentions[0].ban(reason = ' '.join(reason), delete_message_days = 0)
+    if ctx.message.author.guild_permissions.kick_members:
+        try:
+            for tBan in objStore.tBanList:
+                if tBan.userID == user.id:
+                    embed.description = "This user is already in the banlist. Get your shit together, kachigga."
+            else:
                 try:
-                    with open('tempBans.txt', 'wb') as tBans:
-                        pickle.dump(objStore.tBanList, tBans)
-                    tBans.close()
-                except (ValueError, KeyError, TypeError) as exce:
-                    print("Pickle error in tempBan:" + str(exce))
-                    embed.add_field(name = "Error:", value = exce)
-            except Exception as exce:
-                print(exce)
-        embed.description = "**{}** has been successfully banned for **{}** days.\nReason: *{}*".format(user, duration, ' '.join(reason))
-        embed.color = 0x00ff00
-    except Exception as exce:
-        print(exce)
-        embed.description = "An error has occurred when trying to ban **{}**. If you're reading this, 1) Congrats on fucking up the bot, 2) Contact Xaereus about it.".format(user)
-        embed.add_field(name = "Error:", value = exce)
-
-    except MissingPermissions:
+                    objStore.tBanList.append(TempBan(user.id, duration, ' '.join(reason), ctx.message.guild.id, round(time.time())))
+                    #await user.ban(reason = ' '.join(reason), delete_message_days = 0)
+                    try:
+                        with open('tempBans.txt', 'wb') as tBans:
+                            pickle.dump(objStore.tBanList, tBans)
+                        tBans.close()
+                        embed.description = "**{}** has been successfully banned for **{}** days.\nReason: *{}*".format(user.name, duration, ' '.join(reason))
+                        embed.color = 0x00ff00
+                    except Exception as exce:
+                        print("Pickle error in tempBan:" + str(exce))
+                        embed.add_field(name = "Error:", value = exce)
+                except Exception as exce:
+                    print(exce)
+                    embed.description = "An error has occurred when trying to ban **{}**. If you're reading this, 1) Congrats on fucking up the bot, 2) Contact Xaereus about it.".format(user.name)
+        except Exception as exce:
+            print(exce)
+            embed.description = "An error has occurred when trying to ban **{}**. If you're reading this, 1) Congrats on fucking up the bot, 2) Contact Xaereus about it.".format(user.name)
+            embed.add_field(name = "Error:", value = exce)
+    else:
         embed.description = "You do not have permission to use this command. Begone, ***thot***."
     await ctx.send(embed = embed)
 
 @bot.command()
-@commands.has_permissions(kick_members = True)
 async def veiwTBans(ctx):
     """Displays everyone who has been tempbanned."""
     embed = discord.Embed(title = "Command: viewTBans", color = 0x0000ff)
-    for tBan in objStore.tBanList:
-        if tBan.guild == ctx.message.guild:
-            embed.add_field(name = tBan.user.name, value = str(tBan.duration, tban.startTime), inline = False)
-
-    except MissingPermissions:
+    if ctx.message.author.guild_permissions.kick_members:
+        for tBan in objStore.tBanList:
+            if tBan.guildID == ctx.message.guild.id:
+                embed.add_field(name = client.get_user(tBan.userID).name, value = str(tBan.duration, tban.startTime), inline = False)
+    else:
         embed.description = "You do not have permission to use this command. Begone, ***thot***."
         embed.color = 0xff0000
     await ctx.send(embed = embed)
 
+@bot.command()
+async def tempRole(ctx, user : discord.Member, duration, role : discord.Role):
+    """Grants a user a role for a time."""
+    embed = discord.Embed(title = "Command: tempRole", color = 0xff0000)
+    if ctx.message.author.guild_permissions.manage_roles:
+        try:
+            for tRole in objStore.tRoleList:
+                if tRole.userID == user.id and tRole.roleID == role.id:
+                    embed.description = "This user already has that role."
+            else:
+                try:
+                    objStore.tRoleList.append(TempRole(user.id, duration, role.id, ctx.message.guild.id, round(time.time())))
+                    #await user.add_roles(role.id)
+                    try:
+                        with open('tempRoles.txt', 'wb') as tRoles:
+                            pickle.dump(objStore.tRoleList, tRoles)
+                        tRoles.close()
+                        embed.description = "**{}** has been successfully given **{}** for **{}** days.".format(user.name, role.name, duration)
+                        embed.color = 0x00ff00
+                    except Exception as exce:
+                        print("Pickle error in tempRole:" + str(exce))
+                        embed.add_field(name = "Error:", value = exce)
+                except Exception as exce:
+                    print(exce)
+                    embed.description = "An error occurred when trying to give **{}** **{}**. If you're reading this, 1) Congrats on fucking up the bot, 2) Contact Xaereus about it.".format(user.name, role.name)
+        except Exception as exce:
+            print(exce)
+            embed.description = "An error occurred when trying to give **{}** **{}**. If you're reading this, 1) Congrats on fucking up the bot, 2) Contact Xaereus about it.".format(user.name, role.name)
+            embed.add_field(name = "Error:", value = exce)
+    else:
+        embed.description = "You do not have permission to use this command, {}. Begone, ***thot***.".format(user.mention)
+    await ctx.send(embed = embed)
 
 # Start up the bot
 bot.run(magicWord)
